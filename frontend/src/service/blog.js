9a5fc5c275b1738a8a0d366385268d1f82aa9703
vishal rajbhar <vishalrajbharvr01@gmail.com>
2026-07-19 17:00:42 +0530

    blog

--- frontend/src/service/blog.js
+++ frontend/src/service/blog.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 
 export const addBlog = async (data) => {
-    let response = await axios.post('https://infinity-hub-4.onrender.com/addblog', data)
+    let response = await axios.post('https://infinity-hub-2.onrender.com/addblog', data)
     return response;
 }
 
 export const getAllBlog = async () => {
-    let response = await axios.get('https://infinity-hub-4.onrender.com/allblog')
+    let response = await axios.get('https://infinity-hub-2.onrender.com/allblog')
     return response;
 }
 
 export const deleteBlog = async (id) => {
     const response = await axios.delete(
-        `https://infinity-hub-4.onrender.com/deleteblog/${id}`
+        `https://infinity-hub-2.onrender.com/deleteblog/${id}`
     );
 
     return response;
@@ -21,16 +21,16 @@ export const deleteBlog = async (id) => {
 
 export const getfeatchBlogDetails = async (id) => {
     return axios.get(
-        `https://infinity-hub-4.onrender.com/getblogbyid/${id}`
+        `https://infinity-hub-2.onrender.com/getblogbyid/${id}`
     );
 }
 
 export const editBlog = async (id, data) => {
-    let response = await axios.patch(`https://infinity-hub-4.onrender.com/editblog/${id}`, data)
+    let response = await axios.patch(`https://infinity-hub-2.onrender.com/editblog/${id}`, data)
     return response;
 }
 
 export const viewBlog = async (id, data) => {
-    let response = await axios.get(`https://infinity-hub-4.onrender.com/viewblog/${id}`, data)
+    let response = await axios.get(`https://infinity-hub-2.onrender.com/viewblog/${id}`, data)
     return response;
 }
